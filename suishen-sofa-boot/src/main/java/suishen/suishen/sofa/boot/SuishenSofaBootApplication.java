a847630a59937172dff57c4f5cbb9040b1746e98
liweiyuan <liweiyuan@etouch.cn>
2019-07-10 15:48:03 +0800

    init:初始化项目

---
+++ suishen-sofa-boot/src/main/java/suishen/suishen/sofa/boot/SuishenSofaBootApplication.java
@@ -0,0 +1,13 @@
+package suishen.suishen.sofa.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuishenSofaBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuishenSofaBootApplication.class, args);
+    }
+
+}
